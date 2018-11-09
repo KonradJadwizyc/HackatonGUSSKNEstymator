ea79d976dec95d385b26c1de18cbc1841de03185
APiechocka <36155723+APiechocka@users.noreply.github.com>
2018-11-09 22:07:14 +0100

    Start menu with images

--- ui.R
+++ ui.R
@@ -5,7 +5,7 @@ fluidPage(
   
   navbarPage("SGD", position = "fixed-top", theme = shinytheme("united"),
              tabPanel("Start", source("ui/startUI.R")$value),
-             tabPanel("Edukacja", source("ui/edukacjaUI.R")$value),
+             tabPanel("Education", source("ui/edukacjaUI.R")$value),
              tabPanel("Quiz", source("ui/quizUI.R")$value)
   )
 )
