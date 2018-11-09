c5f2ed2e9ed758a03c8bd87fbfe0da8ac30502b5
APiechocka <36155723+APiechocka@users.noreply.github.com>
2018-11-09 19:46:49 +0100

    added tabs

--- ui.R
+++ ui.R
@@ -2,12 +2,10 @@
 fluidPage(
   useShinyjs(),
   br(),br(),br(),
-  selectInput("frontCityName", 
-              label = NULL, 
-              choices = c("Amsterdam", "Berlin", "Vienna")), 
-  navbarPage("iDash", position = "fixed-top", theme = shinytheme("united"),
-             tabPanel("Find your place", source("ui/searchUI.R")$value),
-             tabPanel("Overview", source("ui/overviewUI.R")$value),
-             tabPanel("Live data", source("ui/liveUI.R")$value)
+  
+  navbarPage("SGD", position = "fixed-top", theme = shinytheme("united"),
+             tabPanel("Start", source("ui/startUI.R")$value),
+             tabPanel("Edukacja", source("ui/edukacjaUI.R")$value),
+             tabPanel("Quiz", source("ui/quizUI.R")$value)
   )
 )
