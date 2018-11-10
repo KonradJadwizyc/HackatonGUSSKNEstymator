6247a0c3c4544ce9d52d7c29562818cb2249be64
WojciechDopieralski <wojciech.dopieralski@op.pl>
2018-11-10 03:04:18 +0100

    Zakomentowany Start Tab - przyspiesza testy

--- ui.R
+++ ui.R
@@ -4,7 +4,7 @@ fluidPage(
   br(),br(),br(),
   
   navbarPage("SGD", position = "fixed-top", theme = shinytheme("united"),
-             tabPanel("Start", source("ui/startUI.R")$value),
+             #tabPanel("Start", source("ui/startUI.R")$value),
              tabPanel("Education", source("ui/edukacjaUI.R")$value),
              tabPanel("Quiz", source("ui/quizUI.R")$value)
   )
