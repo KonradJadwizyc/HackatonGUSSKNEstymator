f35ad823dbceb8a35f5210a44c853af6ac76ceba
WojciechDopieralski <wojciech.dopieralski@op.pl>
2018-11-10 14:17:50 +0100

    Proba dodania 2 okienek do wprowadzanie danych, poki co porzucone

--- server/modals.R
+++ server/modals.R
@@ -1,28 +1,55 @@
 modalStartName <- reactiveValues(name="Dear user")
-modalUserAge <- reactiveValues()
+# modalStartCountry <- reactiveValues(country="Wasteland")
+#modalUserAge <- reactiveValues()
 
-observe({
+observe( {
 showModal(
   modalDialog(
-    "What is your name",
-    textInput("modalUserName", label = "Name..."),
+    "What is your name?",
+    textInput("modalUserName", 
+              label = "Name..."),
     
-    footer = div(actionButton(inputId = "done", "Done", style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
+    footer = div(actionButton(inputId = "done", 
+                              "Done", 
+                              style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
                  modalButton("Ignore")),
    
     size = "m", # s, l
     easyClose = T,
     fade = T
   )
-)  
-
-  
+)
 })
+
+# observe({
+# showModal(
+# modalDialog(
+#   "Where are You from?",
+#   textInput("modalUserCountry",
+#             label = "Country..."),
+#   
+#   footer = div(actionButton(inputId = "done_country",
+#                             "Done",
+#                             style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
+#                modalButton("Ignore")),
+#   
+#   size = "m", # s, l
+#   easyClose = T,
+#   fade = T
+# )
+# )
+# })
 
 observeEvent(input$done, {
   modalStartName$name <- input$modalUserName
   removeModal()
-}
-)
+})
+
+# observeEvent(input$done_country, {
+#   modalStartCountry$country <- input$modalUserCountry
+#   removeModal()
+# })
+
 
 output$modalName <- renderText({ modalStartName$name})
+# output$modalCountry <- renderText({ modalStartCountry$country})
