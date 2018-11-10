53d82903a744fbbe7ff7fdaa26a3ea1d08f0f6ac
WojciechDopieralski <wojciech.dopieralski@op.pl>
2018-11-10 05:11:49 +0100

    Dodano id navbarPage
    Dodano tabpanel Quiz 2, ktory bazowo ma byc ukryty

--- ui.R
+++ ui.R
@@ -1,11 +1,15 @@
 
+
+
 fluidPage(
   useShinyjs(),
   br(),br(),br(),
+
   
-  navbarPage("SGD", position = "fixed-top", theme = shinytheme("united"),
+  navbarPage("SGD", id = "navbar", position = "fixed-top", theme = shinytheme("united"),
              #tabPanel("Start", source("ui/startUI.R")$value),
              tabPanel("Education", source("ui/edukacjaUI.R")$value),
-             tabPanel("Quiz", source("ui/quizUI.R")$value)
+             tabPanel("Quiz", source("ui/quizUI.R")$value),
+             tabPanel("Quiz2", source("ui/quiz2UI.R")$value)
   )
 )
