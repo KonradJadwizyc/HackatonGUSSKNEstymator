8e003fd9d5dcbc062990987df1b51f2da0ab8a6e
WojciechDopieralski <wojciech.dopieralski@op.pl>
2018-11-10 11:17:01 +0100

    Poprawa bledow konfliktow

--- ui/edukacjaUI.R
+++ ui/edukacjaUI.R
@@ -7,7 +7,6 @@ sidebarLayout(
         h4(textOutput("modalName"))),
     div(id = "upper_panel",
         selectInput(
-<<<<<<< HEAD
           inputId = "Choose_SDG",
           label = "Choose SDG",
           choices = (c("Zero Hunger",
@@ -25,18 +24,13 @@ sidebarLayout(
           inputId = "searchDateRange",
           label = "Arrivial / Departure",
           start = today(),
-          end = today() + 1
-=======
+          end = today() + 1,
           inputId = "gender",
-<<<<<<< HEAD
           label = "Choose gender",
-          choices = (c("woman", "man"))
->>>>>>> 100e288666dc426cc9516920be4e2866da9a048e
-=======
+          choices = (c("woman", "man")),
           label = "Choose SDG",
           choices = (c("Zero hunger", "Gender equality")),
           selected = "Gender equality"
->>>>>>> 2f77fad220f854e5f33096a861171055872ccff3
         )),
     
       div(id = 'hidden_part',
@@ -54,15 +48,10 @@ sidebarLayout(
           radioButtons(
             inputId = "Choose_topics",
             label = "Choose an interesting theme:",
-<<<<<<< HEAD
-<<<<<<< HEAD
             choices = "ome",
-=======
+
             choices = c("% of women in managarial positions", "% of women in government", "% of women in"),
->>>>>>> 100e288666dc426cc9516920be4e2866da9a048e
-=======
             choices = c("% of women in managarial positions", "% of women in government"),
->>>>>>> 2f77fad220f854e5f33096a861171055872ccff3
             selected = NULL
           ))),
     
