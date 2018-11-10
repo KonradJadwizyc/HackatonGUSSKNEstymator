357d25307cf073a7a4c5c11ac69ee63d3161f69b
WojciechDopieralski <wojciech.dopieralski@op.pl>
2018-11-10 11:33:12 +0100

    komitujcie to :/ bo ja juz nie chce naprawiac :'(
    Blad byl gdzies w servie i UI edukacji

--- server/render_plotly.R
+++ server/render_plotly.R
@@ -1,18 +1,12 @@
-<<<<<<< HEAD
-
-
-=======
 observeEvent(input$img_1, {
   updateTabsetPanel(session, "navbar",
                     selected = "Education")
   
 })
->>>>>>> 100e288666dc426cc9516920be4e2866da9a048e
 
 data <- read.csv2("data.csv", sep = ",")
 data_manager <- data %>% filter(SeriesDescription == "Proportion of women in managerial positions (%)")
 library(tidyverse)
-
 data_manager <- data_manager[c(7, 8, 9)]
 data_manager$Value <- as.numeric(as.character(data_manager$Value))
 data_poland <- data_manager %>% filter(GeoAreaName == "Poland")
@@ -21,30 +15,10 @@ data_inter <- reactive(
 )
 
 output$plot_poland <- renderPlotly(
-plotly::plot_ly(data_poland, x= ~TimePeriod, y=~Value, type = "scatter", mode = "lines")
+  plotly::plot_ly(data_poland, x= ~TimePeriod, y=~Value, type = "scatter", mode = "lines")
 )
 
 output$plot_inter <- renderPlotly(
   p <- plot_ly(data = data_inter()) %>% 
     add_lines( x = ~TimePeriod, y = ~Value, color = ~GeoAreaName, mode = "lines", line = list(width = 3))
 )
-########
-
-glod <- read.csv("data (2).csv")
-glod$Value <- as.numeric(as.character(glod$Value))
-glod_World <- glod %>%
-  filter(SeriesDescription == "Prevalence of undernourishment (%)")
-
-glod_poland <- glod_World %>% filter(GeoAreaName == "Poland")
-data_inter <- reactive(
-  glod_World %>%  filter(GeoAreaName == input$searchCountry)
-)
-
-output$plot_poland <- renderPlotly(
-  p <- plot_ly(data = data_inter()) %>% 
-    add_lines( x = ~TimePeriod, y = ~Value, color = ~GeoAreaName, mode = "lines", line = list(width = 3))
-)
-observeEvent(input$Choose_SDG, {
-  updateRadioButtons(session = session, Choose_topics, choices = c("% of women in ", "% of women in", "% of women in"))
-})
-
--- ui/edukacjaUI.R
+++ ui/edukacjaUI.R
@@ -1,60 +1,36 @@
 data <- read.csv2("data.csv", sep = ",")
 data_manager <- data %>% filter(SeriesDescription == "Proportion of women in managerial positions (%)")
-glod <- read.csv("data (2).csv")
 sidebarLayout(
   sidebarPanel(
     div(id = "panel",
         h4(textOutput("modalName"))),
     div(id = "upper_panel",
         selectInput(
-          inputId = "Choose_SDG",
-          label = "Choose SDG",
-          choices = (c("Zero Hunger",
-                       "Gender Equality",
-                       "No Poverty",
-                       "Climate Action",
-                       "",
-                       "",
-                       "",
-                       "",
-                       "")),
-          selected = "Zero Hunger"
-        ),
-        dateRangeInput(
-          inputId = "searchDateRange",
-          label = "Arrivial / Departure",
-          start = today(),
-          end = today() + 1,
           inputId = "gender",
-          label = "Choose gender",
-          choices = (c("woman", "man")),
           label = "Choose SDG",
           choices = (c("Zero hunger", "Gender equality")),
           selected = "Gender equality"
         )),
     
-      div(id = 'hidden_part',
-          #uiOutput("searchPriceUI"),
-          selectizeInput(
-            inputId = "searchCountry",
-            label = "Choose countries to compare:",
-            choices = c(as.character(unique(data_manager$GeoAreaName))),
-            multiple = TRUE,
-            selected = "Poland",
-            options = list(maxItems = 5,
-                           placeholder = "Choose countries (max. 5)")
-          ),
-          
-          radioButtons(
-            inputId = "Choose_topics",
-            label = "Choose an interesting theme:",
-            choices = "ome",
-
-            choices = c("% of women in managarial positions", "% of women in government", "% of women in"),
-            choices = c("% of women in managarial positions", "% of women in government"),
-            selected = NULL
-          ))),
-    
+    div(id = 'hidden_part',
+        #uiOutput("searchPriceUI"),
+        selectizeInput(
+          inputId = "searchCountry",
+          label = "Choose countries to compare:",
+          choices = c(as.character(unique(data_manager$GeoAreaName))),
+          multiple = TRUE,
+          selected = "Poland",
+          options = list(maxItems = 5,
+                         placeholder = "Choose countries (max. 5)")
+        ),
+        
+        radioButtons(
+          inputId = "Choose interesting topics",
+          label = "Choose an interesting theme:",
+          choices = c("% of women in managarial positions", "% of women in government"),
+          selected = NULL
+        ))),
+  
   mainPanel(
     tabsetPanel(
       tabPanel(
