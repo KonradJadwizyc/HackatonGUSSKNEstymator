b5c75e679741147b270a52dd8e59211f5488d396
APiechocka <36155723+APiechocka@users.noreply.github.com>
2018-11-24 12:41:50 +0100

    All panels connected to plot selectInput

--- server/start_server.R
+++ server/start_server.R
@@ -1,68 +1,85 @@
 observeEvent(input$img_1, {
   updateTabsetPanel(session, "navbar",
                     selected = "Education")
+  updateSelectInput(session, "data_panel", selected = "No poverty")
 })
 observeEvent(input$img_2, {
   updateTabsetPanel(session, "navbar",
                     selected = "Education")
+  updateSelectInput(session, "data_panel", selected = "Zero hunger")
 })
 observeEvent(input$img_3, {
   updateTabsetPanel(session, "navbar",
                     selected = "Education")
+  updateSelectInput(session, "data_panel", selected = "Good health and well-being")
 })
 observeEvent(input$img_4, {
   updateTabsetPanel(session, "navbar",
                     selected = "Education")
+  updateSelectInput(session, "data_panel", selected = 'Quality education')
 })
 observeEvent(input$img_5, {
   updateTabsetPanel(session, "navbar",
                     selected = "Education")
+  updateSelectInput(session, "data_panel", selected = 'Gender equality')
 })
 observeEvent(input$img_6, {
   updateTabsetPanel(session, "navbar",
                     selected = "Education")
+  updateSelectInput(session, "data_panel", selected = 'Clean water and sanitation')
 })
 observeEvent(input$img_7, {
   updateTabsetPanel(session, "navbar",
                     selected = "Education")
+  updateSelectInput(session, "data_panel", selected = 'Affordable and clean energy')
 })
 observeEvent(input$img_8, {
   updateTabsetPanel(session, "navbar",
                     selected = "Education")
+  updateSelectInput(session, "data_panel", selected = 'Decent work and economic growth')
 })
 observeEvent(input$img_9, {
   updateTabsetPanel(session, "navbar",
                     selected = "Education")
+  updateSelectInput(session, "data_panel", selected = 'Industry, innovation and infrastructure')
 })
 observeEvent(input$img_10, {
   updateTabsetPanel(session, "navbar",
                     selected = "Education")
+  updateSelectInput(session, "data_panel", selected = 'Reduced inequality')
 })
 observeEvent(input$img_11, {
   updateTabsetPanel(session, "navbar",
                     selected = "Education")
+  updateSelectInput(session, "data_panel", selected = 'Sustainable cities and communities')
 })
 observeEvent(input$img_12, {
   updateTabsetPanel(session, "navbar",
                     selected = "Education")
+  updateSelectInput(session, "data_panel", selected = 'Responsible consumption and production')
 })
 observeEvent(input$img_13, {
   updateTabsetPanel(session, "navbar",
                     selected = "Education")
+  updateSelectInput(session, "data_panel", selected = 'Climate action')
 })
 observeEvent(input$img_14, {
   updateTabsetPanel(session, "navbar",
                     selected = "Education")
+  updateSelectInput(session, "data_panel", selected = 'Life below water')
 })
 observeEvent(input$img_15, {
   updateTabsetPanel(session, "navbar",
                     selected = "Education")
+  updateSelectInput(session, "data_panel", selected = 'Life on land')
 })
 observeEvent(input$img_16, {
   updateTabsetPanel(session, "navbar",
                     selected = "Education")
+  updateSelectInput(session, "data_panel", selected = 'Peace,  justice and strong institutions')
 })
 observeEvent(input$img_17, {
   updateTabsetPanel(session, "navbar",
                     selected = "Education")
+  updateSelectInput(session, "data_panel", selected = 'Partnership for the goals')
 })
