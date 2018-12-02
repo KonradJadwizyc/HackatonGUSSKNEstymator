7ae69fc91f3291dd65c70c20fa2a7075e06dfa50
WojciechDopieralski <wojciech.dopieralski@op.pl>
2018-12-02 14:37:12 +0100

    Dodano wczytywanie danych, usunieto zakomentowana linijke z UI

--- global.R
+++ global.R
@@ -9,7 +9,16 @@ library(shinythemes)
 library(shinyWidgets)
 library(plotly)
 library(shinyjs)
+library(openxlsx)
 
 My_SDG <- My_SDG
 
 My_SDG$Value <- as.numeric((My_SDG$Value))
+
+pyt <- read.xlsx(xlsxFile = "Pytania.xlsx", 
+                 colNames = TRUE) %>%
+  mutate(praw=as.logical(praw))
+
+# save(pyt, file="pytania.RData")
+
+
