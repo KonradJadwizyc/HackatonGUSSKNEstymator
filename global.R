20b3c3eac1f542f0f3386359d7b1f4c7743ea8c5
WojciechDopieralski <wojciech.dopieralski@op.pl>
2018-12-03 21:37:13 +0100

    Dodanie tekstu opisujacego quiz
    Wycentrowanie radiobuttonow (pytan i odpowiedzi)
    
    Usuniecie bledu, ktory powodwal, ze dopiero po 2 kliknieciu na odp wyswietlalo sie okno o zakonczeniu danego poziomu.
    
    Blad pozostal przy ostatnim pytaniu w quizie :'(

--- global.R
+++ global.R
@@ -12,17 +12,18 @@ library(shinyjs)
 library(openxlsx)
 
 # My_SDG <- read.csv("SDG.csv", sep = ",", dec=".")
-# 
 # My_SDG <- My_SDG %>%
 #   mutate(Value=as.numeric(levels(Value))[Value])
-# 
 # saveRDS(My_SDG, file = "My_SDG.rds")
 
 My_SDG <- readRDS("My_SDG.rds")
 
 # My_SDG %>%
-#   filter(Indicator=="1.1.1", GeoAreaName == "Poland")
+# filter(Indicator=="1.1.1", GeoAreaName == "Poland")
 
+
+
+#Zapisujemy pytania pobrane ze specjalnie przygotowanego pliku xlsx. Zmieniamy 2 kolumny data framu na wartości logiczne! 
 pyt <- read.xlsx(xlsxFile = "Pytania.xlsx",
                  colNames = TRUE) %>%
   mutate(praw=as.logical(praw)) %>%
@@ -30,14 +31,12 @@ pyt <- read.xlsx(xlsxFile = "Pytania.xlsx",
 
 
 
+
 # save(pyt, file="pytania.RData")
-
 summary(My_SDG)
-
 My_SDG <- My_SDG %>% filter(Value != "", !is.na(Value),
-                         GeoAreaName != "", !is.na(GeoAreaName))
+                     GeoAreaName != "", !is.na(GeoAreaName))
 summary(My_SDG)
-
 
 #11284 to są NA
 
--- ui/quizUI.R
+++ ui/quizUI.R
@@ -1,8 +1,11 @@
-#i18n$t
 
+
+#dodanie lewego paska (menu na karcie quiz) i zmiana szerokości
 sidebarLayout(
   sidebarPanel (width = 4,
+                #Dodanie ramek oddzielających od siebie odpowiednie elementy widoczne przez uzytkownika
                 wellPanel(
+                  #Dodanie radio buttonow z wyborem poziomu trudnosci, zmiana jego szerokości.
                   h3(("Difficult lvl:")),
                   radioButtons(inputId = 'question_lvl', 
                                label =  NULL, 
@@ -10,6 +13,7 @@ sidebarLayout(
                                choiceValues = c(1,2,3),
                                width = "50%")
                 ),
+                #Dodanie w ramce inputu na pokazywanie poziomu expa, klikniecie go jest opcjonalne
                 wellPanel(
                   checkboxInput(inputId = "exp_gain",
                                 label = ("Show exp gain"),
@@ -17,15 +21,22 @@ sidebarLayout(
                 )
   ), 
   
+  #sekcja mieszczaca sie na glownej stronie
   mainPanel(
     
+    #Pole dla obrazu
     div(htmlOutput(outputId = "obrazek"), style="text-align: center;"),
+    #Pole dla pytania
     div(htmlOutput(outputId = "pytanie"), style="text-align: center;"),
+    #Dodanie action buttona, odpowiadajacego za odpowiedzi
     div(actionButton(inputId = "answer", label = ("ANSWER"), width = "50%"), style="text-align: center;"),
+    #Wyswietlenie informacji o poprawnosci odpowiedzi 
     div(textOutput("answer"), style="text-align: center;"),
+    #Tester, ktory pokazuje, ze zliczanie dziala - w wersji ostatecznej do wyrzucenia (zakomentowania - przyda sie przy testowaniu czy wszystko chodzi)
     div(textOutput("count_test"), style="text-align: center;"),
+    #Pokazywanie ile xp daje dana odpowiedz
     div(textOutput("answer_xp"), style="text-align: center;"),
-    div(textOutput("count_test_xp"), style="text-align: center;"),
-    htmlOutput(outputId = "usuwanie")
+    # Tet pokazujacy, ze system zlicza expa zdobytego przez danego uzytkownika 
+    div(textOutput("count_test_xp"), style="text-align: center;")
   )
 )
