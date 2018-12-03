7169969272d7ae831e4d30163dc1039b3c04f2b8
WojciechDopieralski <wojciech.dopieralski@op.pl>
2018-12-03 23:44:18 +0100

    Dodano zgodnie z wymaganiami

--- global.R
+++ global.R
@@ -1,24 +1,21 @@
+
 library(shiny)
 library(lubridate)
 library(leaflet)
 library(readr)
 library(dplyr)
+#library(DT)
 library(ggplot2)
 library(shinythemes)
 library(shinyWidgets)
 library(plotly)
 library(shinyjs)
 library(openxlsx)
+library(shiny.i18n)
 
-# My_SDG <- read.csv("SDG.csv", sep = ",", dec=".")
-# My_SDG <- My_SDG %>%
-#   mutate(Value=as.numeric(levels(Value))[Value])
-# saveRDS(My_SDG, file = "My_SDG.rds")
-
-My_SDG <- readRDS("My_SDG.rds")
-
-# My_SDG %>%
-# filter(Indicator=="1.1.1", GeoAreaName == "Poland")
+My_SDG <- readRDS("My_SDG")
+My_SDG <- My_SDG %>%
+  mutate(Value=as.numeric(levels(Value))[Value])
 
 
 
